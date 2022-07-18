65b99260e6f2e0382d2b8fae292e276c811e92a0
ximuli <ximuli666@163.com>
2022-07-18 22:09:01 +0800

    使用TSX代替Template

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Vue 3 + TypeScript + Vite
+
+如果使用 ref 声明变量，那变量名最好以 ref 开头。
+
+例如：
+
+```
+const refCount = ref(0)
+```
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { defineComponent, ref } from 'vue';
+
+export const App = defineComponent({
+  setup() {
+    const refCount = ref(0)
+    const onClick = () => {
+      refCount.value += 1
+    }
+    return () => <>
+      <div>{refCount.value}</div>
+      <button onClick={onClick}>+++</button>
+    </>
+  }
+})
