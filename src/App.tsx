5af8d687e314f3d4692ebbaa3f73af761062cbec
ximuli <ximuli666@163.com>
2022-07-19 21:44:20 +0800

    引入VueRouter4

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import { defineComponent, ref } from 'vue';
 
 export const App = defineComponent({
   setup() {
-    const refCount = ref(0)
-    const onClick = () => {
-      refCount.value += 1
-    }
     return () => <>
-      <div>{refCount.value}</div>
-      <button onClick={onClick}>+++</button>
+      <header>导航
+        <ul>
+          <li>
+            <router-link to="/">Foo</router-link>
+          </li>
+          <li>
+            <router-link to="/about">Bar</router-link>
+          </li>
+        </ul>
+      </header>
+      <div>
+        <router-view></router-view>
+      </div>
+      <footer>页脚</footer>
     </>
   }
 })
