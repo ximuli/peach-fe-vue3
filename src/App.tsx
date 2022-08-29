429bb9ea4c56e53e66f4b5ae93537015d813f42e
leyili <ximuli666@163.com>
2022-08-29 21:58:22 +0800

    CSS初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
+import { RouterView } from 'vue-router';
 
 export const App = defineComponent({
   setup() {
     return () => <>
-      <header>导航
-        <ul>
-          <li>
-            <router-link to="/">Foo</router-link>
-          </li>
-          <li>
-            <router-link to="/about">Bar</router-link>
-          </li>
-        </ul>
-      </header>
-      <div>
-        <router-view></router-view>
-      </div>
-      <footer>页脚</footer>
+      <RouterView/>
     </>
   }
 })
--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,10 +1,19 @@
 import { defineComponent } from 'vue';
 import { RouterView } from 'vue-router';
+import s from './Welcome.module.scss'
+import logo from './../assets/icons/mangosteen.svg'
 
 export const Welcome = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div><RouterView/></div>
+      <div class={s.wrapper}>
+        <header>
+          <img src={ logo } alt="logo" />
+          <h1>山竹记账</h1>
+        </header>
+        <main><RouterView /></main>
+        <footer></footer>
+      </div>
     )
   }
 })
