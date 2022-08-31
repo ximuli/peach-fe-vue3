325c483adc65379641dcff30ccd513591b007cf5
leyili <ximuli666@163.com>
2022-08-31 22:18:14 +0800

    修改默认路由

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,18 +1,16 @@
 import { Welcome } from "../views/Welcome";
-import { Bar } from "../views/Bar";
-import { Foo } from "../views/Foo";
 
 import { First } from "../components/welcome/First";
 import { Forth } from "../components/welcome/Forth";
 import { Second } from "../components/welcome/Second";
 import { Third } from "../components/welcome/Third";
 
-export const routes = [
-  { path: '/', component: Foo },
-  { path: '/about', component: Bar },
+export const routes: RouterRecordRaw[] = [
+  { path: '/', redirect: 'welcome' },
   {
     path: '/welcome',
     component: Welcome,
+    redirect: '/welcome/1',
     children: [
       { path: '1', component: First },
       { path: '2', component: Second },
